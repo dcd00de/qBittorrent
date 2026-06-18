4be0397947c580c4a7a44413cbb93eef53445ac2
Christian G <47645344+dcd00de@users.noreply.github.com>
2026-06-18 22:48:42 +0200

    fix private

--- src/base/bittorrent/torrentimpl.cpp
+++ src/base/bittorrent/torrentimpl.cpp
@@ -732,8 +732,8 @@ void TorrentImpl::replaceTrackers(QList<TrackerEntry> trackers)
 
     // Clear the peer list if it's a private torrent since
     // we do not want to keep connecting with peers from old tracker.
-    if (isPrivate())
-        clearPeers();
+    /*if (isPrivate())
+        clearPeers();*/
 
     deferredRequestResumeData();
     m_session->handleTorrentTrackersReset(this, oldEntries, trackers);
